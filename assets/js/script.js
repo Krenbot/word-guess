7039c638c8bad576164c5baf8ee7895f4ae5737c
Steve Krenn <stevekrenn@gmail.com>
2022-09-19 20:27:46 -0500

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// Saving data
+// localStorage.setItem('myName', 'Josh')
+// localStorage.setItem('favLang', 'Javascript')
+
+// reading data
+// var myName = localStorage.getItem('myName')
+// console.log(myName)
+
+// var favLang = localStorage.getItem('favLang')
+// console.log(favLang)
+
+// Deleting data
+// localStorage.removeItem('favLang')
+// localStorage.clear()
