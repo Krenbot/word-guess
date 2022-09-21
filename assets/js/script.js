55d1f7ff6b1795076846110ab95cddb01bdffbc2
Steve Krenn <stevekrenn@gmail.com>
2022-09-21 18:18:36 -0500

    changed word variable in display to word (array)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,21 @@
 var startBtn = document.querySelector('.start-button')
 var wordBlanksEl = document.querySelector('.word-blanks')
+var scoreEl = document.querySelector('.score')
+
 var words = ["javascript", "variable", "function", "object", "python", "localstorage", "timeout", "interval"]
 var word
-var validChars = "abcdefghijklmopqrstuvwxyz"
+var validChars = "abcdefghijklmnopqrstuvwxyz"
 var guessedCharacters = []
+var score = 0
+
+function checkWord (){
+    console.log(word, wordBlanksEl)
+    //compare word and empty word
+}
+
 
 function handleKeydown(event){
-    console.log(event.key)
-    if (validChars.includes(event.key)){
+    if (validChars.includes(event.key)) {
         //keep track of char that was guessed
         guessedCharacters.push(event.key)
         //re-render word blanks.textcontent
@@ -17,26 +25,26 @@ function handleKeydown(event){
 
 function renderCharacters(){
 var string = ""
-    for (var i = 0; i < words.length; i++){
+    for (var i = 0; i < word.length; i++){
         var letter = word[i]
     //if we have guessed the character
-    if (guessedCharacters.includes(letter)) {
+        if (guessedCharacters.includes(letter) ) {
         //push the character into our string - adds letter that we're currently on into the string
-        string += letter
-    } else { 
+            string += letter + ' '
+         } else { 
         //push an _ into string
-        string += '_ '
-    }
-       
+            string += '_ '
+        }
     //if we have guessed the character
         //concat char into string
-        console.log(string)
     }
     //set textContent of wordBlanksEl to be string
     wordBlanksEl.textContent = string.trim()
+    checkWord()
 }
 
 function startRound(){
+    guessedCharacters = []
     //get random words from array
     var randomIndex = Math.floor(Math.random() * words.length)
     word = words[randomIndex]
