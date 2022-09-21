e8e4c0a351fc6afcf0d279ff3ff8b928a429672d
Steve Krenn <stevekrenn@gmail.com>
2022-09-21 17:18:18 -0500

    keydown listener & finish render chars

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,32 +3,38 @@ var wordBlanksEl = document.querySelector('.word-blanks')
 var words = ["javascript", "variable", "function", "object", "python", "localstorage", "timeout", "interval"]
 var word
 var validChars = "abcdefghijklmopqrstuvwxyz"
-
+var guessedCharacters = []
 
 function handleKeydown(event){
     console.log(event.key)
     if (validChars.includes(event.key)){
         //keep track of char that was guessed
-        //re-render word blanks
+        guessedCharacters.push(event.key)
+        //re-render word blanks.textcontent
+        renderCharacters()
     }
 }
 
 function renderCharacters(){
 var string = ""
     for (var i = 0; i < words.length; i++){
+        var letter = word[i]
     //if we have guessed the character
-        //push the character into our string
-    //if we have guessed the character
-        //concat char into string
-    //else 
+    if (guessedCharacters.includes(letter)) {
+        //push the character into our string - adds letter that we're currently on into the string
+        string += letter
+    } else { 
         //push an _ into string
         string += '_ '
+    }
+       
+    //if we have guessed the character
+        //concat char into string
         console.log(string)
     }
     //set textContent of wordBlanksEl to be string
     wordBlanksEl.textContent = string.trim()
 }
-
 
 function startRound(){
     //get random words from array
@@ -38,11 +44,8 @@ function startRound(){
     renderCharacters()
 }
 
-
-
 startBtn.addEventListener("click", startRound)
 document.addEventListener('keydown', handleKeydown)
-
 
 //score variable
 //time left variable
